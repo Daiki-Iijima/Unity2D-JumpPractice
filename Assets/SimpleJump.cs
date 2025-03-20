e3b6b5a9c453bb75137dec4a64ef4a9b66c4b633
djima <daiki.applecreate@gmail.com>
2025-03-20 15:30:33 +0900

    feat : 横移動追加 問題点 ・壁に当たると落下途中手も止まってしまう ・ジャンプ中に横移動の入力をやめても進み続けてしまう

--- Assets/SimpleJump.cs
+++ Assets/SimpleJump.cs
@@ -6,6 +6,7 @@ public class SimpleJump : MonoBehaviour
 {
     [SerializeField] private float jumpVelocity = 8;
     [SerializeField] private float jumpFallMultiplier = 1.5f;
+    [SerializeField] private float moveSpeed = 1.5f;
     
     private Rigidbody2D _rb2d;
     
@@ -25,6 +26,17 @@ public class SimpleJump : MonoBehaviour
         {
             //  上昇中
             _rb2d.gravityScale = 1f;
+        }
+        
+        //  左右移動
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rb2d.velocity = new Vector2(-moveSpeed, _rb2d.velocity.y);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rb2d.velocity = new Vector2(moveSpeed, _rb2d.velocity.y);
         }
         
         bool isJumping = Mathf.Abs(_rb2d.velocity.y) > 0.01f;
